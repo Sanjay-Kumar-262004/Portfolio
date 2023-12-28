a8eed7ffba80b63ca8a140f274f02896ce713ee4
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2023-12-28 22:27:11 +0530

    Project Updated

--- script.js
+++ script.js
@@ -9,12 +9,16 @@ document.addEventListener("DOMContentLoaded", function () {
 document.addEventListener("DOMContentLoaded", function () {
     var para1 = document.querySelector('.para1 span');
     var para2 = document.querySelector('.decode .para2 span');
-    var para3 = document.querySelector('.iot .para3 span');
+    var para3 = document.querySelector('.ecobin .para3 span');
+    var para4 = document.querySelector('.iot .para4 span');
+    var para5 = document.querySelector('.decode .para5 span');
 
     function handleScroll() {
         var para1OffsetTop = para1.offsetTop;
         var para2OffsetTop = para2.offsetTop;
         var para3OffsetTop = para3.offsetTop;
+        var para4OffsetTop = para4.offsetTop;
+        var para5OffsetTop = para5.offsetTop;
 
         var scrollPosition = window.scrollY;
 
@@ -34,6 +38,19 @@ document.addEventListener("DOMContentLoaded", function () {
             para3.classList.add('animate');
         } else {
             para3.classList.remove('animate');
+        }
+
+        if (scrollPosition > para4OffsetTop - window.innerHeight && scrollPosition < para4OffsetTop + para4.offsetHeight) {
+            para4.classList.add('animate');
+        } else {
+            para4.classList.remove('animate');
+        }
+
+        // Add this block for para5
+        if (scrollPosition > para5OffsetTop - window.innerHeight && scrollPosition < para5OffsetTop + para5.offsetHeight) {
+            para5.classList.add('animate');
+        } else {
+            para5.classList.remove('animate');
         }
     }
 
