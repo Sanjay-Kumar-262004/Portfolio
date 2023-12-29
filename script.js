6d921368a9bcbf80591b6cd515f962fbeb6bc346
Sanjay-Kumar-262004 <sanjaykumar.ece26@gmail.com>
2023-12-29 14:01:39 +0530

    Project Updated

--- script.js
+++ script.js
@@ -7,53 +7,54 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 document.addEventListener("DOMContentLoaded", function () {
-    var para1 = document.querySelector('.para1 span');
-    var para2 = document.querySelector('.decode .para2 span');
-    var para3 = document.querySelector('.ecobin .para3 span');
-    var para4 = document.querySelector('.iot .para4 span');
-    var para5 = document.querySelector('.decode .para5 span');
-
-    function handleScroll() {
-        var para1OffsetTop = para1.offsetTop;
-        var para2OffsetTop = para2.offsetTop;
-        var para3OffsetTop = para3.offsetTop;
-        var para4OffsetTop = para4.offsetTop;
-        var para5OffsetTop = para5.offsetTop;
-
-        var scrollPosition = window.scrollY;
-
-        if (scrollPosition > para1OffsetTop - window.innerHeight && scrollPosition < para1OffsetTop + para1.offsetHeight) {
-            para1.classList.add('animate');
-        } else {
-            para1.classList.remove('animate');
-        }
-
-        if (scrollPosition > para2OffsetTop - window.innerHeight && scrollPosition < para2OffsetTop + para2.offsetHeight) {
-            para2.classList.add('animate');
-        } else {
-            para2.classList.remove('animate');
-        }
-
-        if (scrollPosition > para3OffsetTop - window.innerHeight && scrollPosition < para3OffsetTop + para3.offsetHeight) {
-            para3.classList.add('animate');
-        } else {
-            para3.classList.remove('animate');
-        }
-
-        if (scrollPosition > para4OffsetTop - window.innerHeight && scrollPosition < para4OffsetTop + para4.offsetHeight) {
-            para4.classList.add('animate');
-        } else {
-            para4.classList.remove('animate');
-        }
-
-        // Add this block for para5
-        if (scrollPosition > para5OffsetTop - window.innerHeight && scrollPosition < para5OffsetTop + para5.offsetHeight) {
-            para5.classList.add('animate');
-        } else {
-            para5.classList.remove('animate');
-        }
-    }
-
-    window.addEventListener('scroll', handleScroll);
-    handleScroll();
+    var typed = new Typed("#typed-quote", {
+        strings: ['"Education is the most powerful weapon you can use to change the world - Nelson Mandela"'],
+        typeSpeed: 50,
+        backSpeed: 25,
+        loop: true
+    });
 });
+// document.addEventListener("DOMContentLoaded", function () {
+//     var para1 = document.querySelector('.para1 span');
+//     var para2 = document.querySelector('.decode .para2 span');
+//     var para3 = document.querySelector('.ecobin .para3 span');
+//     var para4 = document.querySelector('.iot .para4 span');
+//     var para5 = document.querySelector('.abet .para5 span');
+
+//     function getElementOffsetTop(element) {
+//         var offsetTop = 0;
+//         while (element) {
+//             offsetTop += element.offsetTop;
+//             element = element.offsetParent;
+//         }
+//         return offsetTop;
+//     }
+
+//     function handleScroll() {
+//         var para1OffsetTop = getElementOffsetTop(para1);
+//         var para2OffsetTop = getElementOffsetTop(para2);
+//         var para3OffsetTop = getElementOffsetTop(para3);
+//         var para4OffsetTop = getElementOffsetTop(para4);
+//         var para5OffsetTop = getElementOffsetTop(para5);
+
+//         var scrollPosition = window.scrollY;
+
+//         function addRemoveClass(element, offsetTop) {
+//             if (scrollPosition > offsetTop - window.innerHeight && scrollPosition < offsetTop + element.offsetHeight) {
+//                 element.classList.add('animate');
+//             } else {
+//                 element.classList.remove('animate');
+//             }
+//         }
+
+//         addRemoveClass(para1, para1OffsetTop);
+//         addRemoveClass(para2, para2OffsetTop);
+//         addRemoveClass(para3, para3OffsetTop);
+//         addRemoveClass(para4, para4OffsetTop);
+//         addRemoveClass(para5, para5OffsetTop);
+//     }
+
+//     window.addEventListener('scroll', handleScroll);
+//     handleScroll();
+// });
+
